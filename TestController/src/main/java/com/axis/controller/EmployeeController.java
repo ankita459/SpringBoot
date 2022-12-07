b2d397ca9fa4b7e9b8be980908bb5bf32c240220
ankita459 <ankitapanpatil18pc@student.mes.ac.in>
2022-12-07 18:59:33 +0530

    Add files via upload

---
+++ TestController/src/main/java/com/axis/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.axis.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+ArrayList<Employee> employees =new ArrayList<Employee>();
+
+	
+	public EmployeeController() {
+		employees.add(new Employee(1,"shreya","HR"));
+		employees.add(new Employee(2,"ravi","developer"));
+		employees.add(new Employee(3,"ananya","manager"));
+		
+	}
+	
+	@GetMapping("/getEmployees")
+	public List<Employee> getAllEmployees(){
+		return employees;
+	}
+	
+	@PostMapping("/addEmployee")
+	public String addEmployee(@RequestBody Employee emp) {
+		employees.add(emp);
+		return "employee added";
+		
+	}
+
+}
